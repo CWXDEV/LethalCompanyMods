cefbd5aab1b330937371c93b197edd094fd745bb
CWXDEV <CWXDEV@outlook.com>
2023-12-27 16:19:39 +0000

    disable money patch

--- CWX-LethalCompany/Patches/MoneyPatch.cs
+++ CWX-LethalCompany/Patches/MoneyPatch.cs
@@ -2,12 +2,12 @@
 
 namespace CWX_LethalCompany.Patches
 {
-    [HarmonyPatch(typeof(Terminal), "Update")]
-    internal class MoneyPatch
-    {
-        static void Prefix(Terminal __instance)
-        {
-            __instance.groupCredits = 9999;
-        }
-    }
+    //[HarmonyPatch(typeof(Terminal), "Update")]
+    //internal class MoneyPatch
+    //{
+    //    static void Prefix(Terminal __instance)
+    //    {
+    //        __instance.groupCredits = 9999;
+    //    }
+    //}
 }
